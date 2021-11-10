06169b29855f46f363cd5574563bcf1d212da30f
juand <juanvasquez.tic@ucaldas.edu.co>
2021-11-09 20:23:51 -0500

    Ajuste

--- app/src/main/java/com/example/marketplace/HomeActivity.kt
+++ app/src/main/java/com/example/marketplace/HomeActivity.kt
@@ -1,10 +1,19 @@
 package com.example.marketplace
 
 import android.content.Context
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.EditText
+import android.view.Menu
 import android.widget.TextView
+import com.google.android.material.snackbar.Snackbar
+import com.google.android.material.navigation.NavigationView
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.appcompat.app.AppCompatActivity
+import com.example.marketplace.databinding.ActivityHomeBinding
 import com.facebook.login.LoginManager
 import com.google.firebase.auth.FirebaseAuth
 
@@ -16,22 +25,44 @@ enum class ProviderType{
 
 class HomeActivity : AppCompatActivity() {
 
-
-    private var emailTextView: TextView? = null;
-    private var providerTextView: TextView? = null;
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var binding: ActivityHomeBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
 
-        emailTextView = findViewById(R.id.emailTextView);
-        providerTextView = findViewById(R.id.providerTextView);
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        //setup
+        setSupportActionBar(binding.appBarHome.toolbar)
+
+        binding.appBarHome.fab.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show()
+        }
+
+
+        val drawerLayout: DrawerLayout = binding.drawerLayout
+        val navView: NavigationView = binding.navView
+        val navController = findNavController(R.id.nav_host_fragment_content_home)
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow
+            ), drawerLayout
+        )
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
+
+        //setup Login
+        title=resources.getString(R.string.test_Home)
         val bundle =intent.extras
         val email=bundle?.getString("email")
         val provider=bundle?.getString("provider")
-        setup(email?:"",provider?:"")
+
+        navView.getHeaderView(0).findViewById<TextView>(R.id.emailTextView).text = email;
+        navView.getHeaderView(0).findViewById<TextView>(R.id.providerTextView).text=provider;
 
         //Save Data
         val prefs=getSharedPreferences(resources.getString(R.string.preds_file), Context.MODE_PRIVATE).edit()
@@ -41,11 +72,17 @@ class HomeActivity : AppCompatActivity() {
 
     }
 
-    private fun setup(email:String,provider:String) {
-        title=resources.getString(R.string.test_Home)
-        emailTextView?.text=email
-        providerTextView?.text=provider
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.home, menu)
+        return true
     }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.nav_host_fragment_content_home)
+        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
+    }
+
 
     fun onSignoff(view: android.view.View) {
 
@@ -54,7 +91,8 @@ class HomeActivity : AppCompatActivity() {
         prefs.clear()
         prefs.apply()
 
-        if(providerTextView?.text == ProviderType.FACEBOOK.name){
+        val navView: NavigationView = binding.navView
+        if(navView.findViewById<TextView>(R.id.providerTextView).text == ProviderType.FACEBOOK.name){
             LoginManager.getInstance().logOut()
         }
 
